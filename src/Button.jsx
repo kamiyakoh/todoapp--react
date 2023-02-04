a01b5934c64a978924ed9b22eab9bd3cc4c0f0ca
kamiyakoh <nanayou791@gmail.com>
2023-02-04 18:02:14 +0900

    Button.jsx_cssの記述位置の修正

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
-const btn = css`
-  cursor: pointer;
-  color: var(--color, #fff);
-  background-color: transparent;
-  border-radius: calc(var(--size, 1) * 2px + 2px);
-  border: solid 2px var(--color, #fff);
-  font-size: calc(var(--size, 1) * 1rem);
-  line-height: 1;
-  padding: calc(var(--size, 1) * 2px + 2px);
-  border-width: calc(var(--size, 1) * 2px);
-`;
-
 function Button({ cssName, onClick, children }) {
+  const btn = css`
+    cursor: pointer;
+    color: var(--color, #fff);
+    background-color: transparent;
+    border-radius: calc(var(--size, 1) * 2px + 2px);
+    border: solid 2px var(--color, #fff);
+    font-size: calc(var(--size, 1) * 1rem);
+    line-height: 1;
+    padding: calc(var(--size, 1) * 2px + 2px);
+    border-width: calc(var(--size, 1) * 2px);
+  `;
+
   return (
-    <button type="button" css={[btn, cssName]} onClick={onClick}>
+    <button type='button' css={[btn, cssName]} onClick={onClick}>
       {children}
     </button>
   );
